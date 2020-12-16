af9bfcce45e311f35a9d6642949ad3f285c0de16
Edwon <edwonia@gmail.com>
2020-12-16 18:34:19 +0900

    package 1.0.1, added ignore on awake frame option to ScreenTouchEventRelay

--- Packages/Mobile Tools/Runtime/ScreenTouchEventRelay.cs
+++ Packages/Mobile Tools/Runtime/ScreenTouchEventRelay.cs
@@ -7,22 +7,45 @@ namespace Edwon.MobileTools
 {
     public class ScreenTouchEventRelay : ScreenTouchEventReceiverBase
     {
+        public bool ignoreTouchEventsOnAwakeFrame = true;
+        bool awakeFrame = true;
+
         public UnityEvent onTouchBegin;
         public UnityEvent onTouchUpdate;
         public UnityEvent onTouchEnd;
 
+        void Awake()
+        {
+            if (ignoreTouchEventsOnAwakeFrame)
+                awakeFrame = true;
+        }
+
+        void Update()
+        {
+            awakeFrame = false;
+        }
+
         public override void OnTouchBegin(Vector2 screenPos)
         {
+            if (ignoreTouchEventsOnAwakeFrame && awakeFrame)
+                return;
+
             onTouchBegin.Invoke();
         }
 
         public override void OnTouchUpdate(Vector2 screenPos)
         {
+            if (ignoreTouchEventsOnAwakeFrame && awakeFrame)
+                return;
+
             onTouchUpdate.Invoke();
         }
 
         public override void OnTouchEnd(Vector2 screenPos)
         {
+            if (ignoreTouchEventsOnAwakeFrame && awakeFrame)
+                return;
+
             onTouchEnd.Invoke();
         }
     }
