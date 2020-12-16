3e9b168a14568f4e14d463661c3cddcefbf151e9
Edwon <edwonia@gmail.com>
2020-12-16 19:08:53 +0900

    removing ignore on awake

--- Packages/Mobile Tools/Runtime/ScreenTouchEventRelay.cs
+++ Packages/Mobile Tools/Runtime/ScreenTouchEventRelay.cs
@@ -7,46 +7,22 @@ namespace Edwon.MobileTools
 {
     public class ScreenTouchEventRelay : ScreenTouchEventReceiverBase
     {
-        [Header("to prevent events getting called on awake")]
-        public bool ignoreOnAwake = true;
-        bool awakeFrame = true;
-
         public UnityEvent onTouchBegin;
         public UnityEvent onTouchUpdate;
         public UnityEvent onTouchEnd;
 
-        void Awake()
-        {
-            if (ignoreOnAwake)
-                awakeFrame = true;
-        }
-
-        void Update()
-        {
-            awakeFrame = false;
-        }
-
         public override void OnTouchBegin(Vector2 screenPos)
         {
-            if (ignoreOnAwake && awakeFrame)
-                return;
-
             onTouchBegin.Invoke();
         }
 
         public override void OnTouchUpdate(Vector2 screenPos)
         {
-            if (ignoreOnAwake && awakeFrame)
-                return;
-
             onTouchUpdate.Invoke();
         }
 
         public override void OnTouchEnd(Vector2 screenPos)
         {
-            if (ignoreOnAwake && awakeFrame)
-                return;
-
             onTouchEnd.Invoke();
         }
     }
